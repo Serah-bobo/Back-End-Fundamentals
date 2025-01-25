4ab08eca65992da9a1060e393a40c707803e6c77
Serah  Wambui Ndungu <137044232+Serah-bobo@users.noreply.github.com>
2025-01-25 11:03:05 +0300

    connecting to mongosh

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,29 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connect } from "http2"
-const mongoose=require("mongoose")
-const app=express()
-const port=5000
-const mongoURI=process.env.MONGO_URI
+import express from "express";
+import dotenv from "dotenv";
+const mongoose = require("mongoose");
 
-app.get("/", (req,res)=>{
-  res.send("hello world")
-})
-app.listen(port, ()=>{
-  console.log(`server is running at http://localhost:${port}`)
-})
+dotenv.config(); // Load environment variables from .env file
 
-mongoose.connect()
+const app = express();
+const port = 5000;
+
+//const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI;
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, {
+  })
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error:Error) => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
